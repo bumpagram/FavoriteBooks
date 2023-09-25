f9c8f3c55b8ca8c972eb81a86ae8e5d6364488f5
bumpagram <invoke@kxdigital.ru>
2023-09-25 19:03:34 +0300

    step 1 complete

---
+++ NewBookFormTVC.swift
@@ -0,0 +1,46 @@
+//  NewBookFormTVC.swift
+//  FavoriteBooks
+//
+//  Created by Denis Azarkov on 9/25/23.
+//
+import UIKit
+
+class NewBookFormTVC: UITableViewController {
+    
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var authorField: UITextField!
+    @IBOutlet var genreField: UITextField!
+    @IBOutlet var lengthField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    @IBAction func pressedSaveButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
