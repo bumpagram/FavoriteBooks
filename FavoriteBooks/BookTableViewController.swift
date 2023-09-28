d67e7bc39cbb33d3e056eab99d57c23112a894f6
bumpagram <invoke@kxdigital.ru>
2023-09-28 15:59:50 +0300

    пересоздал сигвей в сториборде, теперь он ВК-ВК, переписал часть кода, убрал флаги. Добавил методы didSelectRowAt и prepare(for segue), теперь при нажатии на ячейку открывается второй экран=детали, ДАННЫЕ НАКОНЕЦ ТО ВЫГРУЖАЮТСЯ ИЗ ЯЧЕЙКИ

--- FavoriteBooks/BookTableViewController.swift
+++ FavoriteBooks/BookTableViewController.swift
@@ -14,7 +14,6 @@ class BookTableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
         tableView.reloadData()
     }
 
@@ -35,6 +34,7 @@ class BookTableViewController: UITableViewController {
 
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         .delete  // этот метод не обязательный, можно было и удалить, по умолчанию все равно также .delete
     }
@@ -42,7 +42,7 @@ class BookTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             books.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.deleteRows(at: [indexPath], with: .automatic) // теперь можно удалять ячейки
         }
     }
     
@@ -51,11 +51,17 @@ class BookTableViewController: UITableViewController {
         books.insert(movedItem, at: destinationIndexPath.row)  // вставить в массив на конкретную позицию ранее выдернутый элемент
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // наблюдатель срабатывает при нажатии на ячейку
+        let pressedCell = books[indexPath.row]
+        performSegue(withIdentifier: "showDetail", sender: pressedCell)
+    }
+    
+    
 
     // MARK: - Navigation
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
-        print("start prepareForUnwind")
         guard let source = segue.source as? NewBookFormTVC,
               let book = source.newBook else {return}
         
@@ -65,25 +71,29 @@ class BookTableViewController: UITableViewController {
             tableView.deselectRow(at: indexPath, animated: true)
         } else {
             books.append(book)
-            print("after books array append")
-            print(books)
-            print("-force table reload-")
             tableView.reloadData() // строка в этом месте оперативно обновляет экраны
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let willPerform = segue.destination as? NewBookFormTVC else {return}
+            let pressedCell = sender as? Book
+            //print(pressedCell ?? "got nil at pressedCell")  // просто дебаг и наблюдаем
+            willPerform.newBook = pressedCell // передаем в VC форму в проперти newBook нажатую ячейку-сущность, иначе потом гард не пробивается
+        }
+    }
+    
+    
     @IBSegueAction func editBook(_ coder: NSCoder, sender: Any?) -> NewBookFormTVC? {
         let elementToEdit: Book?
         
-        guard let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) else {
+        guard let cell = sender as? UITableViewCell, let indexpath = tableView.indexPath(for: cell) else {
             return nil
         }
-         elementToEdit = books[indexPath.row]
+         elementToEdit = books[indexpath.row]
         return NewBookFormTVC(coder: coder, newBook: elementToEdit)
     }
-    
-  
-    
     
     
 } // ViewController end
--- FavoriteBooks/NewBookFormTVC.swift
+++ FavoriteBooks/NewBookFormTVC.swift
@@ -1,7 +1,7 @@
 //  NewBookFormTVC.swift
 //  FavoriteBooks
 //  Created by Denis Azarkov on 9/25/23.
-//
+
 import UIKit
 
 class NewBookFormTVC: UITableViewController {
@@ -26,36 +26,29 @@ class NewBookFormTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
         updateView()
     }
     
     
     func updateView() {
-        guard let newBook = newBook else {return}
-        titleField.text = newBook.title
-        authorField.text = newBook.author
-        genreField.text = newBook.genre
-        lengthField.text = newBook.length
+        guard let unwrappedBook = newBook else {return}
+        titleField.text = unwrappedBook.title
+        authorField.text = unwrappedBook.author
+        genreField.text = unwrappedBook.genre
+        lengthField.text = unwrappedBook.length
     }
 
     
     @IBAction func pressedSaveButton(_ sender: UIButton) {
         //print("-before gard")
         guard let title = titleField.text, let author = authorField.text, let genre = genreField.text, let length = lengthField.text  else {return}
-        //print("-after gard")
-        //print("--- before init")
-        //print("title is empty? - \(title.isEmpty), count = \(title.count)")
+        guard title != "", author != "", genre != "", length != "" else {return}  // второй гард, тк получается можно создавать экземпляры с пустыми полями
+        
         newBook = Book(title: title, author: author, genre: genre, length: length)
-        //print("--- after init,- ", newBook)
-        //print("title is empty? - \(title.isEmpty), count = \(title.count)")
-        //print(title == nil)
         performSegue(withIdentifier: "manualUnwindSeg", sender: self)
     }
+    
     
     @IBAction func hideKeyboard(_ sender: UITapGestureRecognizer) {
         titleField.resignFirstResponder()
@@ -64,9 +57,7 @@ class NewBookFormTVC: UITableViewController {
         lengthField.resignFirstResponder()
     }
     
-    
-    
-    
+  
     
     
     /*
